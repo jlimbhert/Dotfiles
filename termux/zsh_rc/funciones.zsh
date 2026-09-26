a0be4717606f14eea2b7abfb7679a9bcd3f48d3f
jlimbhert <jlimbhert@tutamail.com>
2026-09-26 02:01:19 -0500

    Auto-sync desde localhost

--- termux/zsh_rc/funciones.zsh
+++ termux/zsh_rc/funciones.zsh
@@ -1,3 +1,5 @@
+
+# Función para copiar contenido de los aechivos
 jcp() {
     if [[ -e "$1" ]]; then
         if cat "$1" | termux-clipboard-set; then
@@ -7,5 +9,34 @@ jcp() {
         fi
     else
         echo -e "El archivo '$1' no existe."
+    fi
+}
+
+# Función para intercambiar archivos entre almacenamiento de android y termux
+jmv() {
+    if [[ -n "$1" ]] && [[ -e "$2" ]]; then
+        case "$1" in
+            droid)
+                if [[ -e "$DIR_ALMACEN_DROID" ]]; then
+                    mv "$2" "$DIR_ALMACEN_DROID"
+                else
+                    echo -e "Error en la ruta de destino."
+                fi
+                ;;
+
+            tux) 
+                if [[ -e "$DIR_ALMACEN_TER" ]]; then
+                    mv "$2" "$DIR_ALMACEN_TER"
+                else
+                    echo -e "Error en la ruta de destino."
+                fi
+                ;;
+
+            *)
+                echo -e "Error al ingresar los datos, inténtelo nuevamente."
+                ;;
+        esac
+    else
+        echo "Error al ingresar los parametros."
     fi
 }
